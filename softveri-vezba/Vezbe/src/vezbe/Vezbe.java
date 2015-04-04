44dec308cd7819be057a3b1034d1f036538d2515
unknown <milicazaricfon@gmail.com>
2015-04-04 20:17:34 +0200

    initial commit

---
+++ softveri-vezba/Vezbe/src/vezbe/Vezbe.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vezbe;
+
+/**
+ *
+ * @author zare1_000
+ */
+public class Vezbe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //odraditi prve vezbe ovde :)
+        System.out.println("Moj prvi program sa ovim izgledom!");
+    }
+
+}
